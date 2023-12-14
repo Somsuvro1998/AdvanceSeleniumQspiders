6fac94ad980814b3043234d66f8a1875e2203599
Somsuvro1998 <email>
2023-12-14 23:00:10 +0530

    Commit

---
+++ src/test/java/Practice/Practice.java
@@ -0,0 +1,24 @@
+package Practice;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Practice {
+
+	public static void main(String[] args) throws Throwable {
+
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver=new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://www.youtube.com/");
+		Thread.sleep(3000);
+		driver.quit();		
+		
+		
+		
+		
+	}
+
+}
